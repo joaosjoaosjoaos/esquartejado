edef17f9662912828571ad972f7458925005051f
João Pedro dos Santos <joaosjoaosjoaos@icloud.com>
2022-11-08 11:36:04 -0300

    saving player history to a file

--- jogadores.py
+++ jogadores.py
@@ -3,10 +3,17 @@ palavras = []
 erros = []
 pontos = []
 
+with open("jogadores.txt", "w", encoding="utf-8") as arquivo:
+    for jogador in jogadores:
+        arquivo.write(f"{jogador}\n")
+with open("jogadores.txt", "r", encoding="utf-8") as file:
+    jogadores = []
+    for i in file.readlines():
+        jogador = i.strip()
+        jogadores.append(jogador)
 
 def mostrar_ranking_jogadores():
-    print(f"""
-    -*- Ranking de Jogadores -*-
-    {jogadores, palavras, erros, pontos}
-    """)
+    print("-*- Ranking de Jogadores -*-")
+    for jogador in jogadores:
+        print(f"{jogador}: {pontos[jogadores.index(jogador)]} pontos, palavra: {palavras[jogadores.index(jogador)]}, erros: {erros[jogadores.index(jogador)]}")
 
